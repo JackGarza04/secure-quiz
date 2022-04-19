65c3dd8b70d0200a6d9da7934b396c61e4207251
P1 <jack@garzafamily.org>
2022-04-19 09:50:09 -0700

    Changes

--- webapp.py
+++ webapp.py
@@ -9,33 +9,48 @@ app = Flask(__name__)
 # This is something random you generate.  
 # For more info see: https://flask.palletsprojects.com/en/1.1.x/config/#SECRET_KEY
 
-app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.  
-                                     #The value should be set in Heroku (Settings->Config Vars).  
-                                     #To run locally, set in env.sh and include that file in gitignore so the secret key is not made public.
+app.secret_key=os.environ["2u8VI6VypGAvdSBpFEPsVw"]; #This is an environment variable.  
+                                                     #The value should be set in Heroku (Settings->Config Vars).  
+                                                     #To run locally, set in env.sh and include that file in gitignore so the secret key is not made public.
+
 
 @app.route('/')
 def renderMain():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/startOver')
-def startOver():
+@app.route('/retake')
+def retake():
     session.clear() #clears variable values and creates a new session
     return redirect(url_for('renderMain')) # url_for('renderMain') could be replaced with '/'
 
-@app.route('/page1')
-def renderPage1():
-    return render_template('page1.html')
+@app.route('/question1')
+def renderQuestion1():
+    return render_template('question1.html')
 
-@app.route('/page2',methods=['GET','POST'])
-def renderPage2():
-    session["firstName"]=request.form['firstName']
-    session["lastName"]=request.form['lastName']
-    return render_template('page2.html')
+@app.route('/question2',methods=['GET','POST'])
+def renderQuestion2():
+    session["answer1"]=request.form['answer1']
+    return render_template('question2.html')
 
-@app.route('/page3',methods=['GET','POST'])
-def renderPage3():
-    session["favoriteColor"]=request.form['favoriteColor']
-    return render_template('page3.html')
+@app.route('/question3',methods=['GET','POST'])
+def renderQuestion3():
+    session["answer2"]=request.form['answer2']
+    return render_template('question3.html')
+    
+@app.route('/question4',methods=['GET','POST'])
+def renderQuestion4():
+    session["answer3"]=request.form['answer3']
+    return render_template('question4.html')
+
+@app.route('/question5',methods=['GET','POST'])
+def renderQuestion5():
+    session["answer4"]=request.form['answer4']
+    return render_template('question5.html')
+    
+@app.route('/results' ,methods=['GET','POST'])
+def renderResults():
+    session["answer5"]=request.form['answer5']
+    return render_template('results.html')
     
 if __name__=="__main__":
     app.run(debug=False)
